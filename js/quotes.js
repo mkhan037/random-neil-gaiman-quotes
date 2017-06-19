b443bb3a7737e1f2d5aa02d2718acf16a5b8a715
thackley <hackley.tara@gmail.com>
2017-06-18 20:02:29 -0500

    Putting it all in GitHub

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+// Required:
+// 1. Create an array of objects named quotes & make it accessible in the global scope
+// 2. Give each object the following properties:
+//    a. quote property that contains the string of the quote
+//    b. source property that is a string of who said the quote (if known)
+//    c. OPTIONAL: citation property that contains the string of where the quote came from (re: a speech, book, etc)
+//    d. OPTIONAL: year property that contains a number of the date of the quote (if known)
+// Additionally:
+// 3. Add an additional property like tags to sort the information into categories
+var quotes = [
+  {
+    quote: "We cannot all succeed when half of us are held back.",
+    source: "Malala Yousafzai",
+    citation: "Address to United Nations",
+    year: 2013,
+    tags: "feminism"
+  },
+  {
+    quote: '<font size="6px">To all the little girls who are watching, never doubt that you are valuable and powerful and deserving of every chance and opportunity in the world to pursue and achieve your own dreams.</font>',
+    source: "Hillary Clinton",
+    citation: "2016 Presidential Concession Speech",
+    year: 2016,
+    tags: "politics"
+  },
+  {
+    quote: '<font size="12px">Of course I am not worried about intimidating men. The type of man who will be intimidated by me is exactly the type of man I have no interest in.</font>',
+    source: "Chimamanda Ngozi Adichie",
+    citation: "We Should All Be Feminsts",
+    year: 2014,
+    tags: "feminism"
+  },
+  {
+    quote: "I am a Woman Phenomenally. Phenomenal Woman, that's me.",
+    source: "Maya Angelou",
+    citation: "Phenomenal Woman",
+    year: 1995,
+    tags: "poetry"
+  },
+  {
+    quote: "A woman with a voice is, by definition, a strong woman.",
+    source: "Melinda Gates",
+    citation: "Powerful Voices Luncheon",
+    year: 2003,
+    tags: "feminism"
+  },
+   {
+    quote: "Women belong in the house — and the Senate.",
+    source: "Unknown",
+    tags: "politics"
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,63 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// Required:
+// 1. Create a function named getRandomQuote that:
+//    a. selects a random quote object from the quotes array
+//    b. returns the randomly selected quote object
+function getRandomQuote ( ) {
+  var upperLimit = quotes.length;
+  console.log("upperLimit", upperLimit);
+  var randNum = Math.floor(Math.random() * upperLimit);
+  console.log("randNum", randNum);
+  return quotes[randNum];
+}
+
+
+// Required:
+// 1. create a function that calls getRandomQuote & stores it in a new variable
+// 2. construct a string containing the info within the object
+//  a. add the quote and the source to the string
+//  b. IF the object has a citation and/or year add that to the string
+// 3. display the final HTML string on the screen with using this snippet:
+//  document.getElementById('quote-box').innerHTML = message;
+function printQuote() {
+  var randQuote = getRandomQuote();
+  console.log(randQuote);
+  var message = '<p class="quote">' + randQuote.quote + '</p>';
+  message += '<p class="source">' + randQuote.source;
+
+  if (randQuote.citation && randQuote.year) {
+    message += '<span class="citation"> ' + randQuote.citation + '</span>';
+    message += '<span class="year"> ' + randQuote.year + '</span></p>';
+  } else if (randQuote.citation) {
+    message += '<span class="citation"> ' + randQuote.citation + '</span></p>';
+  } else if (randQuote.year) {
+    message += '<span class="year"> ' + randQuote.year + '</span></p>';
+  }
+
+  console.log(message);
+  document.getElementById('quote-box').innerHTML = message;
+
+}
+
+
+// Change the background color (& button color) when a new quote is printed
+function getNewBackgroundColor() {
+    printQuote();
+  var newBackgroundColor = "rgb(" + Math.floor(Math.random() * 256 + 1) + ", " + Math.floor(Math.random() * 256 + 1) + ", " + Math.floor(Math.random() * 256 + 1) + ")";
+  document.body.style.backgroundColor = newBackgroundColor;
+  document.getElementById("loadQuote").style.background = newBackgroundColor;
+
+}
+
+getNewBackgroundColor();
+
+
+// Refresh the quote after 10 seconds
+ function refreshQuote() {
+  var quoteRefresh = window.setInterval(printQuote, 10000);
+}
+
+refreshQuote();
